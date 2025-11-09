482089733917928b56dfe2af979139e9ae101e4e
IslamHamada <Islamhamada222@gmail.com>
2025-11-09 13:55:01 +0100

    Rename the main function

--- src/test/java/com/islamhamada/petshop/ServiceRegistryBoot.java
+++ src/test/java/com/islamhamada/petshop/ServiceRegistryBoot.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServiceRegistry {
+class ServiceRegistryBoot {
 
 	@Test
 	void contextLoads() {
